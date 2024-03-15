78ab97791ed8385a143baeb4bb22a3a2a91fd403
zhongwen <zh579385@dal.ca>
2024-03-14 23:56:11 -0300

    made some modifications

--- lab6/src/App.js
+++ lab6/src/App.js
@@ -19,24 +19,32 @@ function App() {
             alt="Question Mark"
             id="questionMark"
           />
-          <Star image="images/harden.jpg" alt="James Harden" id="JamesHarden" />
+          <Star
+            image="https://cdn.nba.com/manage/2020/12/GettyImages-1189867189-scaled-e1607459557816-1536x963.jpg"
+            alt="James Harden"
+            id="JamesHarden"
+          />
 
           <Star
-            image="images/antetokounmpo.png"
+            image="https://www.sportsnet.ca/wp-content/uploads/2023/08/Milwaukee-Bucks-forward-Giannis-Antetokounmpo-1040x572.jpg"
             alt="Giannis Antetokounmpo"
             id="GiannisAntetokounmpo"
           />
           <Star
-            image="images/curry.png"
+            image="https://a57.foxnews.com/static.foxnews.com/foxnews.com/content/uploads/2023/03/1440/810/Stephen-Curry2.jpg?ve=1&tl=1"
             alt="Stephen Curry"
             id="StephenCurry"
           />
           <Star
-            image="images/westbrook.png"
+            image="https://www.ocregister.com/wp-content/uploads/2024/01/Nets-Clippers-Basketball-4.jpg?w=1080"
             alt="Russell Westbrook"
             id="RussellWestbrook"
           />
-          <Star image="images/jokic.png" alt="Nikola Jokic" id="NikolaJokic" />
+          <Star
+            image="https://ca-times.brightspotcdn.com/dims4/default/9852807/2147483647/strip/true/crop/3126x2084+0+0/resize/1200x800!/format/webp/quality/75/?url=https%3A%2F%2Fcalifornia-times-brightspot.s3.amazonaws.com%2F75%2F8e%2F3b42ce38351644a9f79801b58c50%2F57dd6438113e41f6b95dded0cdaa6754"
+            alt="Nikola Jokic"
+            id="NikolaJokic"
+          />
         </div>
       </div>
     </div>
